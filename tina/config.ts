2430f5dbaed63cc7385f839ea55794b63e1a57ae
Francisco Leonel Moreno Eraso <f.moreno@aligntech.com>
2023-11-21 23:46:51 +0100

    chore: testing empty path for public folder for media

--- tina/config.ts
+++ tina/config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   media: {
     tina: {
       mediaRoot: "assets",
-      publicFolder: `${process.env.PROD ? "birmap" : "public"}`,
+      publicFolder: "",
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
